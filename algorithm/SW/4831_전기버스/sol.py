263a8110de319c3807f577df0ac194bc15c1c653
kimhyunso <gustn9515@gmail.com>
2022-12-27 00:11:56 +0900

    20221227 24:11

--- algorithm/SW/4831_전기버스/sol.py
+++ algorithm/SW/4831_전기버스/sol.py
@@ -8,10 +8,20 @@ for tc in range(1, T+1):
     K, N, M = map(int, input().split())
     charges = list(map(int, input().split()))
     bus_stations = [n for n in range(N+1)]
-    while K >= N+1:
+    result = 0
+    for charge in charges:
+        bus_stations[charge] = 'o'
+
+    print(bus_stations)
+
+    for idx in range(0, len(bus_stations), K):
+       pass
+
+    print(f'#{tc} {result}')
 
 
-        K += K
+
+
 
 
     # print()
