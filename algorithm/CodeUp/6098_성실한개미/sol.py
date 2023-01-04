4123bf9f7cc5aec703186a7f364eb73f2eb18f2b
hyunsooKim <gustn9515@gmail.com>
2023-01-04 17:51:21 +0900

    20230104 17:51

--- algorithm/CodeUp/6098_성실한개미/sol.py
+++ algorithm/CodeUp/6098_성실한개미/sol.py
@@ -3,13 +3,21 @@ sys.stdin = open('input.txt')
 
 matrix = [list(map(int, input().split())) for _ in range(10)]
 
-row = col = 2 - 1
+start = end = 1
 
-while matrix[row][col] != 2:
-    while matrix[row][col] != 1:
+for row in range(start, len(matrix)):
+    if matrix[row][end+1] == 2:
+        matrix[row][end+1] = 9
+        break
+    for col in range(end, len(matrix)):
+        if matrix[row][col] == 1:
+            end = col-1
+            break
         matrix[row][col] = 9
-        col += 1
-    row += 1
+
+
+for m in matrix:
+    print(*m)
 
 
 
